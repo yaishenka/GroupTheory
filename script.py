9bf1f2872c2fafc15dd279961b31af4e17be4e3e
yaishenka <gagarinovdaniil@gmail.com>
2020-01-05 18:30:12 +0300

    fix travis

--- script.py
+++ script.py
@@ -1,12 +1,18 @@
 import os
 
-sources_dir = 'src/'
-compiled_dir = 'compiled/'
+def compile_files():
+	sources_dir = 'src/'
+	compiled_dir = 'compiled/'
 
-file_list = os.listdir(sources_dir)
+	file_list = os.listdir(sources_dir)
 
-for file in file_list:
-	if 'DS_Store' in file: # Костыль
-		continue
-	print(file)
-	os.system('pdflatex -output-directory={0} {1}'.format(compiled_dir, os.path.join(sources_dir, file)))
+	for file in file_list:
+		if 'DS_Store' in file: # Костыль
+			continue
+		print(file)
+		os.system('pdflatex -output-directory={0} {1}'.format(compiled_dir, os.path.join(sources_dir, file)))	
+
+if 'Travis build' not in os.environ['TRAVIS_COMMIT_MESSAGE']:
+	compile_files()
+
+
